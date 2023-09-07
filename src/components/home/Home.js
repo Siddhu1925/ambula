b2aa6861909fcbfb9286583f37487d87224d2706
Siddhu1925 <114770885+Siddhu1925@users.noreply.github.com>
2023-09-07 15:26:42 +0530

    Add files via upload

---
+++ src/components/home/Home.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="home">
+      <div
+        animate={{ y: 100 }}
+        transition={{ ease: "easeOut", duration: 2 }}
+      >
+        <h1 class="dual-color">Ambula</h1>
+        <p className="description">
+          Empowering individuals with a comprehensive digital healthcare solution. 
+
+Seamlessly manage health records, book appointments, and access vital healthcare services with ease. 
+
+Join us in revolutionizing the future of healthcare. #DigitalHealth #HealthTech #Innovation
+        </p>
+      </div>
+      <div className="home-btns">
+        <div
+          className="box"
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.8,
+            delay: 0.5,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+        >
+          <button
+            className="hm-bt"
+            onClick={() => {
+              navigate("/register");
+            }}
+          >
+            Register  <span className="arrow"></span>
+          </button>
+        </div>
+        <div
+          className="box"
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.8,
+            delay: 0.5,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+        >
+          <button
+            className="hm-bt"
+            onClick={() => {
+              navigate("/signin");
+            }}
+          >
+            Signin <span className="arrow"></span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
